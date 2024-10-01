116419d8937b951e096ff225cac7c6a6a1a9508d
MD-Rayhan-kabir <mdrayhankabir126@gmail.com>
2024-10-02 01:21:27 +0600

    js final commit

--- index.js
+++ index.js
@@ -3,46 +3,6 @@ const getElementUsingId = (id)=>{
 
 }
 
-
-
-const showblog = getElementUsingId("showblog");
-const showHistory = getElementUsingId("showHistory");
-const donat_container = getElementUsingId("donat_container");
-const history_container = getElementUsingId("history_container");
-const closeModel = getElementUsingId("closeModel");
-const modal = getElementUsingId("modal");
-
-
-const toggleView = (showElement, hideElement, activeButton, inactiveButton) => {
-    hideElement.classList.add("hidden");
-    showElement.classList.remove("hidden");
-    activeButton.classList.add("bg-btn_bg");
-    inactiveButton.classList.remove("bg-btn_bg");
-};
-
-showblog.addEventListener('click', (e) => {
-    e.preventDefault();
-    toggleView(donat_container, history_container, showblog, showHistory);
-});
-
-showHistory.addEventListener('click', (e) => {
-    e.preventDefault();
-    toggleView(history_container, donat_container, showHistory, showblog);
-});
-
-
-
-showHistory.addEventListener("click", (e) => {
-    e.preventDefault();
-    history_container.classList.add("flex");
-    history_container.classList.remove("hidden");
-    donat_container.classList.add("hidden");
-    showHistory.classList.add("bg-btn_bg");
-    showblog.classList.remove("bg-btn_bg");
-});
-
-
-
 let sum = 5500;
 const mainAmount = getElementUsingId('mainAmount');
 // common function 
@@ -56,19 +16,30 @@ const handleDonation = (inputVal, val, title) => {
         sum -= inputVal;
         val.innerHTML = value += inputVal;
         mainAmount.innerText = sum;
-      
-    }
+        }
+
 };
 //  first 
 const handleClickOne = (e) => {
     let inputAmount1 = getElementUsingId("input_ammount_1");
     const inputField1 = parseInt(getElementUsingId("inputfiled_1").value);
     handleDonation(inputField1, inputAmount1);
+    
 };
 
 const handleClickTwo = (e) => {
     let inputAmount2 = getElementUsingId("input_ammount_2");
     const inputField2 = parseInt(getElementUsingId("inputfiled_2").value);
     handleDonation(inputField2, inputAmount2);
+}
+const handleClickThree = (e) => {
+    let inputAmount3 = getElementUsingId("input_ammount_3");
+    const inputField3 = parseInt(getElementUsingId("inputfiled_3").value);
+    handleDonation(inputField3, inputAmount3);
+}
+const handleClickFour = (e) => {
+    let inputAmount4 = getElementUsingId("input_ammount_4");
+    const inputField4 = parseInt(getElementUsingId("inputfiled_4").value);
+    handleDonation(inputField4, inputAmount4);
 }
 
